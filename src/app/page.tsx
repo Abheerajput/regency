196e648c18671923df701d1a5099291804c98a9d
abheeTanwar <ar0776631@gmail.com>
2024-12-11 16:01:41 +0530

    es

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Image from "next/image";
 import Main from "../Components/Main/Page"
 import '../app/globals.css'
--- src/types/react-slick.d.ts
+++ src/types/react-slick.d.ts
@@ -2,4 +2,3 @@ declare module 'react-slick' {
     const Slider: React.ComponentType<any>;
     export default Slider;
   }
-''  
