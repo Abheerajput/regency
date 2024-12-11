8611ff7cb78f77a9cb6ac82d2bbb0eb5d20b3ba1
abheeTanwar <ar0776631@gmail.com>
2024-12-11 16:47:07 +0530

    code

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,34 @@
-import path from 'path';
+import type { NextConfig } from "next";
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true, // Enables the new `app` directory
-  },
-
-  async rewrites() {
-    return [
-      {
-        source: '/assets/:path*',
-        destination: '/public/assets/:path*',
-      },
-    ];
-  },
-
-  webpack: (config) => {
-    config.resolve.alias['@assets'] = path.resolve(__dirname, 'public/assets');
-    return config;
-  },
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
 export default nextConfig;
+
+// import path from 'path';
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   experimental: {
+//     appDir: true, // Enables the new `app` directory
+//   },
+
+//   async rewrites() {
+//     return [
+//       {
+//         source: '/assets/:path*',
+//         destination: '/public/assets/:path*',
+//       },
+//     ];
+//   },
+
+//   webpack: (config) => {
+//     // Adding alias for public assets
+//     config.resolve.alias['@assets'] = path.resolve(__dirname, 'public/assets');
+//     return config;
+//   },
+// };
+
+// export default nextConfig;
