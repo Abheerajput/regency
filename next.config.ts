fa4836c98a3d98a1d233de8f5ade1577517149c5
abheeTanwar <ar0776631@gmail.com>
2024-12-11 17:10:41 +0530

    code

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true, // Enable app directory support
+  },
   /* config options here */
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+
+        'sx': {'min': '318px', 'max': '576.99px'},
+        'xs': {'min': '318px', 'max': '768.99px'},
+        'sm': {'min': '769px', 'max': '1024.99px'}, 
+        'md': {'min': '1025px', 'max': '1280.99px'},
+        'lg': {'min': '1281px', 'max': '1438.99px'}, 
+        'xl': {'min': '1439.99px'}, 
+       
+      },
+      fontFamily: {
+        inter: ['"Inter"', 'sans-serif'],
+        publicSans: ['"Public Sans"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
-}
-
+} satisfies Config;
